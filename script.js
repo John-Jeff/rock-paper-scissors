2309d766693fcec8287e22dd236b3fdcef65e262
Jeff <jeffreyjohn102@gmail.com>
2022-05-31 19:42:22 -0400

    Modify file with pseudocode

---
+++ script.js
@@ -0,0 +1,13 @@
+//**Pseudocode**//
+//When the game function is started, it will call the play function and keep track of the winner of each play call.
+//During the play function, the user will be prompted to pick (and enter) Rock, Paper, or Scissors.
+//The computer will be given the same options and randomly pick one.
+//The play function will compare the two results and determine the winner based on the rules of Rock Paper Scissors.
+//Once someone wins 5 games from the play function, the game function will end and the winner will be displayed.
+
+//Create function for computerPlay that returns Rock, Paper or Scissors.
+//Create a function that plays a single round and takes two inputs (user input and computer input)
+    //Have the function return win or lose
+//Get user input
+    //Make user input case-insensitive
+//Create function called game that calls play function to play 5 rounds and keeps score
