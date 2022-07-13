b8829e00df44c805971b70629cb59c3b13a63e52
Jeff <jeffreyjohn102@gmail.com>
2022-07-12 20:14:57 -0400

    integrate DOM elements

--- script.js
+++ script.js
@@ -5,70 +5,111 @@
 //The play function will compare the two results and determine the winner based on the rules of Rock Paper Scissors.
 //Once someone wins 5 games from the play function, the game function will end and the winner will be displayed.
 
-let playerScore = 0;    //initalize player score
-let computerScore = 0;  //initialize computer score
+const rock = {
+    win: 'Scissors',
+    lose: 'Paper',
+    tie: 'Rock',
+    button: document.querySelector('#rock-btn')
+}
+
+const paper = {
+    win: 'Rock',
+    lose: 'Scissors',
+    tie: 'Paper',
+    button: document.querySelector('#paper-btn')
+}
+
+const scissors = {
+    win: 'Paper',
+    lose: 'Rock',
+    tie: 'Scissors',
+    button: document.querySelector('#scissor-btn')
+}
+
+let playerScore = document.querySelector('#player-score');
+let computerScore = document.querySelector('#comp-score');
+let result = document.querySelector('.results');
+let resetField = document.querySelector('.reset');
+// let playerScore = 0;    //initalize player score
+// let computerScore = 0;  //initialize computer score
+
+rock.button.addEventListener('click', function (e) {
+    decideWinner(rock);
+})
+
+paper.button.addEventListener('click', function (e) {
+    decideWinner(paper);
+})
+
+scissors.button.addEventListener('click', function (e) {
+    decideWinner(scissors);
+})
 
 //Create function for computerPlay that returns Rock, Paper or Scissors.
 function computerPlay() {
-    const options = ['ROCK', 'PAPER', 'SCISSORS'];
+    const options = ['Rock', 'Paper', 'Scissors'];
     return options[Math.floor(Math.random() * 3)];
 }
 
-//Get user input and check for valid entry
-function playerInput() {
-    let player = prompt('Enter Rock, Paper or Scissors').toUpperCase(); //Make user input case-insensitive
-    while (player !== 'ROCK' && player !== 'PAPER' && player !== 'SCISSORS') {  //Check for valid entry
-        player = prompt('Not a valid option, please enter Rock, Paper or Scissors').toUpperCase();
+function decideWinner(selection) {
+    let comp = computerPlay();
+    if (comp === selection.win) {
+        result.textContent = `${selection.tie} beats ${selection.win}! Player wins this round!`;
+        playerScore.textContent++;
+    } else if (comp === selection.lose) {
+        result.textContent = `${selection.lose} beats ${selection.tie}! Computer wins this round!`;
+        computerScore.textContent++;
+    } else {
+        result.textContent = 'Its a tie! No winner this round!';
     }
-    return player;
+
+    if (playerScore.textContent === '5' || computerScore.textContent ==='5') {
+        rock.button.disabled = true;
+        paper.button.disabled = true;
+        scissors.button.disabled = true;
+
+        if (playerScore.textContent === '5') {
+            result.textContent = 'Congrats! You beat Greg!';
+        } else if (computerScore.textContent === '5') {
+            result.textContent = "Congrats! You're a loser";
+        }
+
+        resetGame();
+    }    
+    
 }
+
+function resetGame() {
+    let resetButton = document.createElement('button');
+    resetButton.textContent = 'Play Again?';
+    resetButton.style.marginTop = '30px';
+    resetButton.classList.add('button');
+    resetField.classList.add('reset-button');
+    resetField.appendChild(resetButton);
+}
+
+// //Get user input and check for valid entry
+// function playerInput() {
+//     let player = prompt('Enter Rock, Paper or Scissors').toUpperCase(); //Make user input case-insensitive
+//     while (player !== 'ROCK' && player !== 'PAPER' && player !== 'SCISSORS') {  //Check for valid entry
+//         player = prompt('Not a valid option, please enter Rock, Paper or Scissors').toUpperCase();
+//     }
+//     return player;
+// }
 
 //Create a function that plays a single round and takes two inputs (user input and computer input)
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'ROCK') {
-        if (computerSelection === 'SCISSORS') {
-            console.log('Rock beats Scissors! Player wins this round!');
-            playerScore += 1;
-        } else if (computerSelection === 'PAPER') {
-            console.log('Paper beats Rock! Computer wins this round!');
-            computerScore += 1;
-        } else {
-            console.log('Its a tie! No winner this round!');
-        }
-    } else if (playerSelection === 'PAPER') {
-        if (computerSelection === 'ROCK') {
-            console.log('Paper beats Rock! Player wins this round!');
-            playerScore += 1;
-        } else if (computerSelection === 'SCISSORS') {
-            console.log('Scissors beats Paper! Computer wins this round!');
-            computerScore += 1;
-        } else {
-            console.log('Its a tie! No winner this round!');
-        }
-    } else if (playerSelection === 'SCISSORS') {
-        if (computerSelection === 'PAPER') {
-            console.log('Scissors beats Paper! Player wins this round!');
-            playerScore += 1;
-        } else if (computerSelection === 'ROCK') {
-            console.log('Rock beats Scissors! Computer wins this round!');
-            computerScore += 1;
-        } else {
-            console.log('Its a tie! No winner this round!');
-        }
-    }
-}
 
 //Create function called game that calls play function to play 5 rounds and keeps score
-function game() {
-    alert('Welcome to a really cool game of Rock Paper Scissors');
-    alert('First to 5 wins. Let the game begin!');
-    while (playerScore !== 5 && computerScore !== 5) {
-        playRound(playerInput(), computerPlay());
-        console.log(`************\nScore:\nPlayer - ${playerScore} \nComputer - ${computerScore}\n************`);
-    }
-    if (playerScore === 5) {
-        alert('Congrats! You are the winner of this really cool game!')
-    } else {
-        alert('Aww man, you lost. Better luck next time, loser.')
-    }
-}
+// function game() {
+//     alert('Welcome to a really cool game of Rock Paper Scissors');
+//     alert('First to 5 wins. Let the game begin!');
+//     while (playerScore !== 5 && computerScore !== 5) {
+//         playRound(playerInput(), computerPlay());
+//         console.log(`************\nScore:\nPlayer - ${playerScore} \nComputer - ${computerScore}\n************`);
+//     }
+//     if (playerScore === 5) {
+//         alert('Congrats! You are the winner of this really cool game!')
+//     } else {
+//         alert('Aww man, you lost. Better luck next time, loser.')
+//     }
+// }
